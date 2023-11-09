7df9a3ba1b218b7146a3961c84265f85dad76cd7
CM-Gimenez <martingimenez2222@gmail.com>
2023-11-09 10:53:49 -0300

    inicio de proyecto

---
+++ src/main/java/com/academia/actividadm4/Entidades/Persona.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.academia.actividadm4.Entidades;
+
+/**
+ *
+ * @author usuario
+ */
+public class Persona {
+    //atributos de la clase person
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    // constructores de clase persona
+    public Persona() {
+    }
+
+    public Persona(int id, String nombre, String apellido, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+    //getters y setters de los atributos de la clase persona
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/academia/actividadm4/Entidades/Task.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.academia.actividadm4.Entidades;
+
+/**
+ *
+ * @author usuario
+ */
+public class Task {
+    
+    private int id;
+    private String title;
+    private String description;
+
+    public Task() {
+    }
+
+    public Task(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+    
+}
